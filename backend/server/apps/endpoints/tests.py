e0ada7dced553c13a8866ff140a50f24be0f038d
Ayo <niran360@gmail.com>
2021-01-24 22:11:07 +0100

    test case for second algorithm added

--- backend/server/apps/endpoints/tests.py
+++ backend/server/apps/endpoints/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 
 # Create your tests here.
+from apps.ml.income_classifier.extra_trees import ExtraTreesClassifier
 from rest_framework.test import APIClient
 
 class EndpointTests(TestCase):
@@ -25,7 +26,29 @@ class EndpointTests(TestCase):
         }
         classifier_url = "/api/v1/income_classifier/predict"
         response = client.post(classifier_url, input_data, format='json')
-        # self.assertEqual(response.status_code, 200)
-        # self.assertEqual(response.data["label"], "<=50K")
-        # self.assertTrue("request_id" in response.data)
-        # self.assertTrue("status" in response.data)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data["label"], "<=50K")
+        self.assertTrue("request_id" in response.data)
+        self.assertTrue("status" in response.data)
+    def test_et_algorithm(self):
+        input_data = {
+        "age": 37,
+        "workclass": "Private",
+        "fnlwgt": 34146,
+        "education": "HS-grad",
+        "education-num": 9,
+        "marital-status": "Married-civ-spouse",
+        "occupation": "Craft-repair",
+        "relationship": "Husband",
+        "race": "White",
+        "sex": "Male",
+        "capital-gain": 0,
+        "capital-loss": 0,
+        "hours-per-week": 68,
+        "native-country": "United-States"
+    }
+    my_alg = ExtraTreesClassifier()
+    response = my_alg.compute_prediction(input_data)
+    self.assertEqual('OK', response['status'])
+    self.assertTrue('label' in response)
+    self.assertEqual('<=50K', response['label'])
